d5063b8a0221423ce0e9de34a7e44038405e4c5d
MoRashid <MoRashid>
2024-07-27 01:33:28 +0100

    deployment fixes

--- src/components/ErrorModal.tsx
+++ src/components/ErrorModal.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { createPortal } from "react-dom";
-import { forwardRef, useImperativeHandle, useRef } from "react";
+import { forwardRef, useEffect, useImperativeHandle, useRef } from "react";
 
 const ErrorModal = forwardRef(function ErrorModal(props, ref) {
   const dialog = useRef<HTMLDialogElement>(null);
@@ -14,17 +14,23 @@ const ErrorModal = forwardRef(function ErrorModal(props, ref) {
     };
   });
 
-  return createPortal(
-    <dialog ref={dialog} className="backdrop:bg-stone-900/90 p-4 rounded-md shadow-md">
-      <h1 className="mb-4 text-stone-500">Please ensure all fields are populated</h1>
-      <form method="dialog" className="text-right">
-        <button className="rounded-md bg-stone-700 px-8 py-2 text-xs text-stone-400 hover:bg-stone-600 hover:text-stone-100 md:text-base mt-6">
-          OK
-        </button>
-      </form>
-    </dialog>,
-    document.body,
-  );
+  let content;
+
+  if (typeof document !== 'undefined') {
+    content = createPortal(
+      <dialog ref={dialog} className="backdrop:bg-stone-900/90 p-4 rounded-md shadow-md">
+        <h1 className="mb-4 text-stone-500">Please ensure all fields are populated</h1>
+        <form method="dialog" className="text-right">
+          <button className="rounded-md bg-stone-700 px-8 py-2 text-xs text-stone-400 hover:bg-stone-600 hover:text-stone-100 md:text-base mt-6">
+            OK
+          </button>
+        </form>
+      </dialog>,
+      document.body
+    );
+  }
+
+  return content
 });
 
 export default ErrorModal;
